1b257e34e98aa3acbfcb7a570bebceff8884fddb
GuillaumeDSM <g.dsm@laposte.net>
2019-04-06 23:37:55 +0200

    added startup message

--- Main.go
+++ Main.go
@@ -14,6 +14,7 @@ func main() {
 	headersOk := handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type"})
 	originsOK := handlers.AllowedOrigins([]string{"*"})
 	methodsOk := handlers.AllowedMethods([]string{"GET", "POST"})
+	log.Printf("starting")
 	log.Fatal(http.ListenAndServe(":8080", handlers.CORS(headersOk, originsOK, methodsOk)(router)))
 
 }
